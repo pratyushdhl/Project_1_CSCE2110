2522971fba7b2e4d0322dd1f82aa725388162d47
semicolonsemicolon <rhodes.ethan2.0@gmail.com>
2026-09-15 15:25:40 -0500

    Input cycle base
    Formatting fixes, placeholders for all inputs and functional exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
-#include "ReservationManager.h"
+#include "../include/ReservationManager.h"
 using namespace std;
 
 int main() {
 
-  while true {
+	bool active = true;
+	int choice;
 
-    int choice;
+	while (active) {
+
+		// User input prompt
+		cout << "===== Campus Resource Reservation System =====" << endl;
+		cout << "\n1. View Resources" << endl;
+		cout << "2. Create Reservation" << endl;
+		cout << "3. Cancel Reservation" << endl;
+		cout << "4. View Waiting Lists" << endl;
+		cout << "5. Undo Cancellation" << endl;
+		cout << "6. Search Reservations" << endl;
+		cout << "7. Sort Resources" << endl;
+		cout << "8. Generate Report" << endl;
+		cout << "9. Exit" << endl;
+		cout << "Enter Choice: ";
+		cin >> choice;
     
-    cout << "===== Campus Resource Reservation System =====" << endl;
-    cout << "\n1. View Resources" << endl;
-    cout << "2. Create Reservation" << endl;
-    cout << "3. Cancel Reservation" << endl;
-    cout << "4. View Waiting Lists" << endl;
-    cout << "5. Undo Cancellation" << endl;
-    cout << "6. Search Reservations" << endl;
-    cout << "7. Sort Resources" << endl;
-    cout << "8. Generate Report" << endl;
-    cout << "9. Exit" << endl;
-    cout << "Enter Choice: ";
-    cin >> choice;
-    while (choice < 1 || choice > 9) {
-      cout << "Invalid choice, please enter 1-9: ";
-      cin >> choice;
-    }
+		// Correct invalid choices
+		while (choice < 1 || choice > 9) {
+			cout << "Invalid choice, please enter 1-9: ";
+			cin >> choice;
+		}
+	
+		// Handle choices: View Resources (1)
+		
+		// Handle choices: Create Reservation (2)
+		
+		// Handle choices: Cancel Reservation (3)
+		
+		// Handle choices: View Waiting Lists (4)
+		
+		// Handle choices: Undo Cancellation (5)
+		
+		// Handle choices: Search Reservations (6)
+		
+		// Handle choices: Sort Resources (7)
+		
+		// Handle choices: Generate Report (8)
+	
+		// Handle choices: Exit (9)
+		if (choice == 9) {
+			active = false;
+		}
+	
   }
   
   return 0;
