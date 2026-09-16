3ca2663555f44940e7cf51235fd031beb1d9779a
Pratyush Dahal <pratyushdahal60@gmail.com>
2026-09-16 14:06:44 -0500

    Added Resource Manager block

---
+++ include/ResouceManager.h
@@ -0,0 +1,26 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <vector>
+#include <string>
+#include "Resource.h"
+
+using namespace std;
+
+class ResourceManager
+{
+    private:
+        vector<Resource> resources;
+    
+    public:
+        // Load resource from resources.txt
+        bool loadResources(const string& filename);
+
+        //Display all resources
+        void displayResources() const;
+
+        // Display resource availability
+        void displayResourceAvailability() const;
+};
+
+#endif
